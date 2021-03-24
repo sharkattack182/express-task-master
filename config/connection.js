055585352a6498d731f195d9a0724a9573c600f3
Matthew Courtney <matthewdc182@gmail.com>
2021-03-23 22:52:09 -0400

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.envJAWSDB_URL)
+} else {
+   var connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
     password: "rootroot",
     database: "tasks_db"
-});
+}); 
+}
 
-connection.connect(function(err) {
-    if(err) {
-        console.error("error connecting: " + err.stack);
-    }
 
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 
 module.exports = connection;
