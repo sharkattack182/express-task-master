6dc4394c8e0b723fa2831099e00d5492713e155e
Matthew Courtney <matthewdc182@gmail.com>
2021-03-23 23:37:22 -0400

    fixing connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 if(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.envJAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
    var connection = mysql.createConnection({
     host: "localhost",
@@ -11,6 +11,7 @@ if(process.env.JAWSDB_URL) {
     database: "tasks_db"
 }); 
 }
+
 
 connection.connect(function (err) {
     if (err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
 INSERT INTO task (task_name, task_description) VALUES ("Wake up", "Wake up, drink water and stretch");
 INSERT INTO task (task_name, task_description) VALUES ("Get ready", "Brush teeth, shower, shave");
 INSERT INTO task (task_name, task_description) VALUES ("Eat Breakfast", "Eat Eggs, bacon and toast");
+INSERT INTO task (task_name, task_description) VALUES ("Go to work", "Drive to work listen to a podcast.");
